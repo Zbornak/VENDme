f55699fae15cd84316026b3b680efe43673acd93
Mark <zbornack@protonmail.com>
2023-02-13 16:15:23 +0000

    Removed unneeded variable in favouritesView

--- VENDme/FavouritesView.swift
+++ VENDme/FavouritesView.swift
@@ -9,7 +9,6 @@ import SwiftUI
 
 struct FavouritesView: View {
     @StateObject var favourites = UserFavourites()
-    @State var currentNumber = 1
     
     var body: some View {
         NavigationView {
