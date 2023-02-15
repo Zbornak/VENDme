3fb67f4a312a92b05859fecf7d5b0ae5812c7e9b
Mark <zbornack@protonmail.com>
2023-02-15 14:21:20 +0000

    Made DirectionsView and ReportView

---
+++ VENDme/DirectionsView.swift
@@ -0,0 +1,20 @@
+//
+//  DirectionsView.swift
+//  VENDme
+//
+//  Created by Mark Strijdom on 15/02/2023.
+//
+
+import SwiftUI
+
+struct DirectionsView: View {
+    var body: some View {
+        Text("Get directions")
+    }
+}
+
+struct DirectionsView_Previews: PreviewProvider {
+    static var previews: some View {
+        DirectionsView()
+    }
+}
---
+++ VENDme/ReportView.swift
@@ -0,0 +1,20 @@
+//
+//  ReportView.swift
+//  VENDme
+//
+//  Created by Mark Strijdom on 15/02/2023.
+//
+
+import SwiftUI
+
+struct ReportView: View {
+    var body: some View {
+        Text("Report a problem")
+    }
+}
+
+struct ReportView_Previews: PreviewProvider {
+    static var previews: some View {
+        ReportView()
+    }
+}
