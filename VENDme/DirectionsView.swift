7533cd09745eeaf6ab5c5502a582bb62c5f5d8f5
Mark <zbornack@protonmail.com>
2023-02-16 10:30:47 +0000

    Created a separate MapView for the DirectionsView

--- VENDme/DirectionsView.swift
+++ VENDme/DirectionsView.swift
@@ -12,69 +12,50 @@ import SwiftUI
 
 struct DirectionsView: View {
     @Environment(\.dismiss) var dismiss
+    
+    @ObservedObject var locationManager: LocationManager
+    
     @State private var currentLocation = ""
     @State private var desiredLocation = ""
-    @State private var region = MKCoordinateRegion(
-        center: CLLocationCoordinate2D(latitude: 36.983341312795126, longitude: 138.25980299484613),
-        span: MKCoordinateSpan(latitudeDelta: 15, longitudeDelta: 15)
-    )
-    @State private var useUserLocation = false
-    
-    @ObservedObject var vendingMachines: VendingMachines
-    @ObservedObject var userFavourites: UserFavourites
-    @ObservedObject var locationManager: LocationManager
     
     var body: some View {
         NavigationView {
-            Form {
-                Section {
-                    HStack {
-                        Text("From:")
-                        TextField("Current location", text: $currentLocation)
-                    }
-                    
-                    HStack {
-                        LocationButton(.shareCurrentLocation) {
-                            locationManager.requestLocation()
-                        }
-                        .cornerRadius(30)
-                        .labelStyle(.iconOnly)
-                        .foregroundColor(.white)
-                        
-                        Text("Use current location")
-                        Spacer()
-                    }
+            VStack {
+                HStack {
+                    Text("From:")
+                    TextField("Current location", text: $currentLocation)
                 }
                 
-                Section {
-                    HStack {
-                        Text("To:")
-                        TextField("To", text: $desiredLocation)
-                        Image(systemName: "arrow.triangle.swap")
+                HStack {
+                    LocationButton(.shareCurrentLocation) {
+                        locationManager.requestLocation()
                     }
-                    .toolbar {
-                        Button("Done") {
-                            dismiss()
-                        }
-                    }
-                }
-            }
+                    .cornerRadius(30)
+                    .labelStyle(.iconOnly)
+                    .foregroundColor(.white)
                     
-            Map(coordinateRegion: useUserLocation ? $locationManager.region : $region, showsUserLocation: true, annotationItems: vendingMachines.machines) { vendingMachine in
-                    MapAnnotation(coordinate: CLLocationCoordinate2D(latitude: vendingMachine.latitude, longitude: vendingMachine.longitude)) {
-                        NavigationLink(destination: ContentView(vendingMachine: vendingMachine, userFavourites: userFavourites)) {
-                                Image(systemName: "lightswitch.off")
-                                .frame(width: 20, height: 20)
-                                .foregroundColor(.black)
+                    Text("Use current location")
+                    Spacer()
+                }
+                
+                HStack {
+                    Text("To:")
+                    TextField("To", text: $desiredLocation)
+                    Image(systemName: "arrow.triangle.swap")
+                }
+                .toolbar {
+                    Button("Done") {
+                        dismiss()
                     }
                 }
             }
+            .padding()
         }
     }
 }
 
 struct DirectionsView_Previews: PreviewProvider {
     static var previews: some View {
-        DirectionsView(vendingMachines: VendingMachines(), userFavourites: UserFavourites(), locationManager: LocationManager())
+        DirectionsView(locationManager: LocationManager())
     }
 }
