95b88d9a3b3bab21fe8c9e12d6cb364a5592b115
Mark <zbornack@protonmail.com>
2023-02-17 16:52:30 +0000

    Minor change

--- VENDme/LocationManager.swift
+++ VENDme/LocationManager.swift
@@ -24,7 +24,6 @@ class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
     }
 
     func requestLocation() {
-        //manager.requestLocation()
         manager.requestAlwaysAuthorization()
     }
 
