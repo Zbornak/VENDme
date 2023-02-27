8138f8a1602dc74803f21e8106093992b5dfca5f
Mark <zbornack@protonmail.com>
2023-02-27 13:54:58 +0000

    Changed AddView when user submits machine

--- VENDme/AddView.swift
+++ VENDme/AddView.swift
@@ -16,86 +16,115 @@ struct AddView: View {
     
     @State private var rating = 3
     
+    @State private var machineSubmitted = false
+    
     var body: some View {
-        Form {
-            Section {
-                Text("Name:")
-            } header: {
-                Text("vending machine name")
-            }
-            
-            Section {
-                Text("Street:")
-                Text("City:")
-                Text("Region:")
-                Text("Country:")
-                Text("Postcode:")
-            } header: {
-                Text("vending machine address")
-            }
-            
-            Section {
-                Button {
-                    //stuff to come here
-                } label: {
-                    HStack {
-                        Image(systemName: "figure.wave")
-                        Text("I'm there now")
-                    }
-                }
-            } header: {
-                Text("use your location")
-            }
-            
-            Section {
-                Picker("What does it sell?", selection: $vendingMachineTypeChoice) {
-                    ForEach(vendingMachineTypes, id: \.self) {
-                        Text($0)
-                    }
+        if machineSubmitted == false {
+            Form {
+                Section {
+                    Text("Name:")
+                } header: {
+                    Text("vending machine name")
                 }
                 
-                Toggle("In use?", isOn: $isInUse)
-            }
-            
-            Section {
-                Text("Enter description here...")
-                    .foregroundColor(.secondary)
-            } header: {
-                Text("brief description (180 words or less)")
-            }
-            
-            Section {
-                HStack {
-                    Text("Rate this machine:")
-                    Spacer()
-                    RatingView(rating: $rating)
+                Section {
+                    Text("Street:")
+                    Text("City:")
+                    Text("Region:")
+                    Text("Country:")
+                    Text("Postcode:")
+                } header: {
+                    Text("vending machine address")
                 }
-            } header: {
-                Text("rate this vending machine")
-            }
-            
-            Button {
-                    //stuff to come here
-            } label: {
-                HStack {
-                    Spacer()
-                    HStack {
-                        Image(systemName: "plus")
-                        Text("Add machine for review")
+                
+                Section {
+                    Button {
+                        //stuff to come here
+                    } label: {
+                        HStack {
+                            Image(systemName: "figure.wave")
+                            Text("I'm there now")
+                        }
+                    }
+                } header: {
+                    Text("use your location")
+                }
+                
+                Section {
+                    Picker("What does it sell?", selection: $vendingMachineTypeChoice) {
+                        ForEach(vendingMachineTypes, id: \.self) {
+                            Text($0)
+                        }
                     }
                     
-                    Spacer()
+                    Toggle("In use?", isOn: $isInUse)
                 }
-            }
+                
+                Section {
+                    Text("Enter description here...")
+                        .foregroundColor(.secondary)
+                } header: {
+                    Text("brief description (180 words or less)")
+                }
+                
+                Section {
+                    HStack {
+                        Text("Rate this machine:")
+                        Spacer()
+                        RatingView(rating: $rating)
+                    }
+                } header: {
+                    Text("rate this vending machine")
+                }
+                
+                Button {
+                    machineSubmitted = true
+                } label: {
+                    HStack {
+                        Spacer()
+                        HStack {
+                            Image(systemName: "plus")
+                            Text("Add machine for review")
+                        }
+                        
+                        Spacer()
+                    }
+                }
                 .fontWeight(.bold)
                 .buttonStyle(.borderedProminent)
                 .tint(.white)
                 .foregroundColor(.black)
                 .overlay(RoundedRectangle(cornerRadius: 25)
-                .stroke(.black, lineWidth: 1))
+                    .stroke(.black, lineWidth: 1))
                 .padding()
+            }
+            .navigationTitle("Add a Vending Machine")
+        } else {
+            VStack {
+                Image("vending_machine_pixel_art")
+                
+                Text("Many thanks for your submission, someone will check it out and it should be live soon!")
+                    .padding()
+                
+                Button {
+                    machineSubmitted = false
+                } label: {
+                    HStack {
+                        HStack {
+                            Image(systemName: "checkmark")
+                            Text("Back")
+                        }
+                    }
+                }
+                .fontWeight(.bold)
+                .buttonStyle(.borderedProminent)
+                .tint(.white)
+                .foregroundColor(.black)
+                .overlay(RoundedRectangle(cornerRadius: 25)
+                    .stroke(.black, lineWidth: 1))
+                .padding()
+            }
         }
-        .navigationTitle("Add a Vending Machine")
     }
 }
 
