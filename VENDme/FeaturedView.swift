a9441e2e7e0bb6481f65fee765c343c040b16f87
Mark <zbornack@protonmail.com>
2023-02-27 16:16:25 +0000

    Working on FeaturedView

--- VENDme/FeaturedView.swift
+++ VENDme/FeaturedView.swift
@@ -8,16 +8,62 @@
 import SwiftUI
 
 struct FeaturedView: View {
+    @ObservedObject var vendingMachines: VendingMachines
+    
+    @ObservedObject var userFavourites: UserFavourites
+    
     var body: some View {
-        VStack {
-            Text("Featured vending machines")
+        GeometryReader { geometry in
+            VStack {
+                List {
+                    ForEach(vendingMachines.machines.filter({ userFavourites.favourites.contains($0.id) })) { vendingMachine in
+                        NavigationLink {
+                            ContentView(vendingMachine: vendingMachine, userFavourites: userFavourites)
+                        } label: {
+                            HStack {
+                                Image(vendingMachine.mainPicture)
+                                    .resizable()
+                                    .cornerRadius(10)
+                                    .frame(maxWidth: geometry.size.width * 0.17, maxHeight: geometry.size.height * 0.07)
+                                    .shadow(radius: 5)
+                                
+                                VStack {
+                                    HStack {
+                                        Text(vendingMachine.name)
+                                            .fontWeight(.bold)
+                                        Spacer()
+                                    }
+                                    
+                                    HStack {
+                                        Text("\(vendingMachine.city), \(vendingMachine.country)")
+                                            .foregroundColor(.secondary)
+                                        Spacer()
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    .onDelete(perform: removeItems)
+                }
+            }
+        }
+        .toolbar {
+            EditButton()
         }
         .navigationTitle("Featured")
+    }
+    
+    func removeItems(at offsets: IndexSet) {
+        let vendingMachines = vendingMachines.machines.filter({ userFavourites.favourites.contains($0.id) })
+        for index in offsets {
+            let vendingMachine = vendingMachines[index]
+            userFavourites.favourites.remove(vendingMachine.id)
+        }
     }
 }
 
 struct FeaturedView_Previews: PreviewProvider {
     static var previews: some View {
-        FeaturedView()
+        FeaturedView(vendingMachines: VendingMachines(), userFavourites: UserFavourites())
     }
 }
