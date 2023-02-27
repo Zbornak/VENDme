79db4ab35a642f1fe587f77e533029a66f140f3a
Mark <zbornack@protonmail.com>
2023-02-27 10:35:36 +0000

    Added enum containing all of the json requirements

--- VENDme/AddVendingMachine.swift
+++ VENDme/AddVendingMachine.swift
@@ -8,10 +8,52 @@
 import Foundation
 
 struct Item: Codable {
+    var id: Int
     var name: String
+    var checkIns: Int
+    var street: String
+    var city: String
+    var region: String
+    var country: String
+    var countryLatitude: Double
+    var countryLongitude: Double
+    var postcode: String
+    var showMachine: Bool
+    var type: Bool
+    var inUse: Bool
+    var description: String
+    var latitude: Double
+    var longitude: Double
+    var userRating: Int
+    var numberOfRatings: Int
+    var mainPicture: String
+    var picture2: String
+    var picture3: String
+    var picture4: String
 
     enum CodingKeys: String, CodingKey {
+        case id
         case name
+        case checkIns
+        case street
+        case city
+        case region
+        case country
+        case countryLatitude
+        case countryLongitude
+        case postcode
+        case showMachine
+        case type
+        case inUse
+        case description
+        case latitude
+        case longitude
+        case userRating
+        case numberOfRatings
+        case mainPicture
+        case picture2
+        case picture3
+        case picture4
     }
 }
 
